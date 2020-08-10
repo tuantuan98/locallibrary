48b5722aced95f5aa16e70d0dd2ed1cc01b0b987
tuantuan98 <dang.hoang.tuan@sun-asterisk.com>
2020-08-10 10:48:23 +0700

    Part 7: Sessions framework

--- catalog/views.py
+++ catalog/views.py
@@ -16,7 +16,9 @@ def index(request):
     # The 'all()' is implied by default.    
     num_authors = Author.objects.count()
     num_genre = Genre.objects.count()
-    
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
@@ -24,6 +26,7 @@ def index(request):
         'num_authors': num_authors,
         'num_genre': num_genre,
         'num_books_contain_word':num_books_contain_word,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
